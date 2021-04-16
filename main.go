d4dbc56e83cd86073977b6671100bb9e96161a7e
baza04 <bazarbay.amir@gmail.com>
2021-04-17 02:19:44 +0600

    feat: added two routes

--- main.go
+++ main.go
@@ -7,12 +7,27 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific Snippet..."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create new Snippet..."))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	server := http.Server{
 		Addr:         ":4000",
