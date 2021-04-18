a7873a65386f364f76f9421037166e0f226551fa
baza04 <bazarbay.amir@gmail.com>
2021-04-18 23:52:00 +0600

    feat: added flag handler

--- cmd/web/app.go
+++ cmd/web/app.go
@@ -1,22 +1,32 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"time"
 )
 
+type config struct {
+	addr, staticDir string
+}
+
 func main() {
+	conf := &config{}
+	flag.StringVar(&conf.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&conf.staticDir, "static", "../../ui/static/", "Path to static assets")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	fileServer := http.FileServer(http.Dir("../../ui/static/"))
+	fileServer := http.FileServer(http.Dir(conf.staticDir))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	server := http.Server{
-		Addr:         ":4000",
+		Addr:         conf.addr,
 		Handler:      mux,
 		ReadTimeout:  time.Second * 10,
 		WriteTimeout: time.Second * 10,
